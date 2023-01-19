7192f44fd25423313fb30c3a37cf43d3371a8bf1
Richard Verkerk <richard@hayfyve.com>
2023-01-19 18:27:46 +0100

    First init
    Vuejs, Tailwind, Vite

---
+++ src/components/jobTitleComp.vue
@@ -0,0 +1,35 @@
+
+<template>
+    {{ jobTitle }}
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobTitles: ['PHP developer', 'Laravel developer', 'Webdeveloper', 'back-end developer', 'front-end developer'],
+            jobTitle: 'front-end developer',
+            count: 0,
+        }
+    },
+    methods: {
+        changeJobTitle: function () {
+
+            setInterval(function () {
+
+                if (this.count == this.jobTitles.length) {
+                    this.count = 0;
+                }
+
+                this.jobTitle = this.jobTitles[this.count];
+
+                this.count++
+            }.bind(this), 1500);
+
+        }
+    },
+    mounted() {
+        this.changeJobTitle()
+    }
+}
+</script>
